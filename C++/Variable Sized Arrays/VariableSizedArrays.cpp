be86f9ff074c5e794165d3fb3df6008610252fed
Said Adem <saidadem3@gmail.com>
2019-02-20 12:29:54 -0600

    variablesizearray init

---
+++ C++/Variable Sized Arrays/VariableSizedArrays.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int rows, colums, problems;
+    int *arr;
+
+    arr = new int[];
+    return 0;
+}
+
